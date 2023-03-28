afd398a61e2bef02fcbe37f93c92c20f8eb9d73a
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-03-28 16:12:23 +0500

    Create Login form

---
+++ Create Login Form with React/src/Component/BasicForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const BasicForm = () => {
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+  const [allEntry, setAllEntry] = useState([]);
+
+  const submitForm = (e)=>{
+    e.preventDefault();
+
+    const newEntry = {email: email , password: password};
+    setAllEntry([...allEntry,newEntry])
+  }
+
+  
+  return (
+    <>
+      <form action="" onSubmit={submitForm}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name='email' id='email' value={email} onChange={(e)=> setEmail(e.target.value)} />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name='password' id='password' value={password} onChange={(e)=> setPassword(e.target.value)} />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+      {allEntry.map((elem)=>{
+        return (
+          <h1>Email: {elem.email} - Password : {elem.password}</h1>
+
+        )
+      })}
+      
+    </>
+  )
+}
+
+export default BasicForm
